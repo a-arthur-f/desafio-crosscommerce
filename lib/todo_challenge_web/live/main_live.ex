10ceed40c15ab94a311af3136e2ac579b1dff7b1
Arthur Fusco <fuscoarthur1@gmail.com>
2022-09-25 19:24:15 -0300

    persistencia de dados adicionada

--- lib/todo_challenge_web/live/main_live.ex
+++ lib/todo_challenge_web/live/main_live.ex
@@ -5,19 +5,30 @@ defmodule TodoChallengeWeb.MainLive do
   end
 
   def handle_event("save", %{"todo" => todo}, socket) do
+
     new_index = socket.assigns.last_index + 1
-    {:noreply, assign(socket,
+    socket = assign(socket,
       todos:  socket.assigns.todos ++ [%{
-        id: new_index,
-        title: todo["title"],
-        done: false}],
+        "id" => new_index,
+        "title" => todo["title"],
+        "done" => false}],
       last_index: new_index
 
-   )}
+   )
+    {:noreply, push_event(socket, "saveTodos", %{todos: socket.assigns})}
   end
 
   def handle_event("delete", %{"id" => id}, socket) do
     id = String.to_integer(id)
-    {:noreply, assign(socket, :todos, socket.assigns.todos |> Enum.filter(fn todo -> todo.id != id end))}
+    socket = assign(socket, :todos, socket.assigns.todos |> Enum.filter(fn todo -> todo["id"] != id end))
+    {:noreply, push_event(socket, "saveTodos", %{todos: socket.assigns})}
+  end
+
+  def handle_event("restoreTodos", %{"todos" => nil}, socket), do: {:noreply, socket}
+
+  def handle_event("restoreTodos", %{"last_index" => last_index}, socket) when not is_integer(last_index), do: {:noreply, socket}
+
+  def handle_event("restoreTodos", %{"todos" => todos, "last_index" => last_index}, socket) do
+    {:noreply, assign(socket, todos: todos, last_index: last_index)}
   end
 end
